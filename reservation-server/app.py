6672fb3fb6af0b946a92977e9dc8fc4e86a7bd59
sangmin-lee-nuvi <sangmin.lee@nuvilabs.com>
2024-08-30 16:59:41 +0900

    added middleware for ec2

--- reservation-server/app.py
+++ reservation-server/app.py
@@ -17,7 +17,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add your React app's URL
+    allow_origins=["http://localhost:3000",
+                   "http://3.36.123.32:3000"],  # Add your React app's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
