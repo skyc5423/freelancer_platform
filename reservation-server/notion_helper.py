0560fe78226327a34998f3bb2b14c999699e32e1
sangmin-lee-nuvi <sangmin.lee@nuvilabs.com>
2024-09-17 17:45:27 +0900

    added filter to db

--- reservation-server/notion_helper.py
+++ reservation-server/notion_helper.py
@@ -17,38 +17,44 @@ class NotionHelper:
 
     @log_function_call
     def _get_all_database_list(self):
-        if not self.cache_database_list:
-            self.cache_database_list = self.notion.search(
-                filter={"property": "object", "value": "database"})['results']
+        self.cache_database_list = self.notion.search(
+            filter={"property": "object", "value": "database"})['results']
         return self.cache_database_list
 
     @log_function_call
     def _get_database_id(self, database_title: str):
-        if not self.cache_database_id:
+        if not self.cache_database_id or database_title not in self.cache_database_id.keys():
             self.cache_database_id = {database['title'][0]['text']['content']: database['id']
                                       for database in self._get_all_database_list()}
         return self.cache_database_id[database_title]
 
     @log_function_call
-    def get_database_with_title(self, database_title: str):
-        return self.notion.databases.query(database_id=self._get_database_id(database_title))
+    def get_database_with_date(self, date: str):
+
+        year = date[:4]
+        month = date[4:6]
+        database_title = f'{year}-{month}'
+
+        filters = {
+            "property": "날짜",
+            "rich_text": {
+                        "contains": f"{date}"
+            }
+        }
+        return self.notion.databases.query(database_id=self._get_database_id(database_title),
+                                           filter=filters)
 
     def convert_string_to_datetime(self, date: str) -> datetime.datetime:
         return datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f%z')
 
     @log_function_call
     def parse_schedule(self, schedule):
-        schedule_date_start = self.convert_string_to_datetime(
-            schedule['properties']['시작 시간']['date']['start'])
-        schedule_date_end = self.convert_string_to_datetime(
-            schedule['properties']['종료 시간']['date']['start'])
-
         schedule = {
-            'date': schedule_date_start.strftime("%Y%m%d"),
+            'date': schedule['properties']['날짜']['rich_text'][0]['plain_text'],
             'class_song': schedule['properties']['수업곡']['title'][0]['plain_text'],
             'class_type': schedule['properties']['수업 종류']['select']['name'],
-            'start_time': schedule_date_start.strftime("%H:%M"),
-            'end_time': schedule_date_end.strftime("%H:%M"),
+            'start_time': schedule['properties']['시작 시간']['rich_text'][0]['plain_text'],
+            'end_time': schedule['properties']['종료 시간']['rich_text'][0]['plain_text'],
             'location_name': schedule['properties']['위치']['rich_text'][0]['plain_text'],
             'max_user_num': schedule['properties']['최대 인원']['number'],
             'user_id_list': [d['id'] for d in schedule['properties']['참가자']['relation']]
@@ -56,16 +62,11 @@ class NotionHelper:
         return schedule
 
     def get_schedule_by_date(self, date: str):
-        year = date[:4]
-        month = date[4:6]
-
-        database_title = f'{year}-{month}'
-        database = self.get_database_with_title(database_title)
+        database = self.get_database_with_date(date)
         schedule_list = []
         for schedule in database['results']:
             parsed_schedule = self.parse_schedule(schedule)
-            if parsed_schedule['date'] == f'{date}':
-                schedule_list.append(Schedule(**parsed_schedule))
+            schedule_list.append(Schedule(**parsed_schedule))
         return schedule_list
 
 
